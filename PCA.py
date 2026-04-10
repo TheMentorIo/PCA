0235eb40015d45938f701b82d80de76e5ad6ada0
TheMentorIo <uoabdalrhman@gmail.com>
2026-04-10 20:44:10 +0200

    implment: PCA and tested it

--- PCA.py
+++ PCA.py
@@ -5,51 +5,49 @@ class PCA():
         # hyperparameter representing the number of dimensions after reduction
         self.new_dim = new_dim
         # for standardization
-        self.μ:np.ndarray
-        self.σ:np.ndarray
+        self.mean:np.ndarray
+        self.std:np.ndarray
         # for PCA
         self.A:np.ndarray 
 
     # x_train is (m,n) matrix where each row is an n-dimensional vector of features
     def fit(self, x_train):
-        # TODO 1: Find μ and σ of each feature in x_train
-        self.μ = None
-        self.σ = None
-        # if a column has zero std (useless constant) set σ=1 (skip their standardization)
-        self.σ = np.where(self.σ == 0, 1, self.σ)
-        
+        # TODO 1: Find mean and std of each feature in x_train
+        self.mean = np.mean(x_train, axis=0)
+        self.std = np.std(x_train, axis=0, ddof=1)
         # TODO 2: Standardize the training data
-        z_train = None
+        z_train = x_train - self.mean
                 
         # TODO 3: Compute the covariance matrix
-        Σ = None
+        cov_matrix = np.cov(z_train,rowvar=False)
         
         # TODO 4: Compute eigenvalues and eigenvectors using Numpy
-        λs, U = None
-        λs, U = λs.real, U.real           # sometimes a zero imaginary part can appear due to approximations
+        eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+        eigenvalues, eigenvectors = eigenvalues.real, eigenvectors.real           # sometimes a zero imaginary part can appear due to approximations
         
         # TODO 5: Sort eigenvalues and eigenvectors
-        # TODO 5.1: Find the sequence of indices that sort λs in descending order
-        sorting_inds = None
-        # TODO 5.2: Use it to sort λs and U
-        λs = None
-        U = None
+        # TODO 5.1: Find the sequence of indices that sort eigenvalues in descending order
+        sorting_inds = np.argsort(eigenvalues)[::-1]
+        # TODO 5.2: Use it to sort eigenvalues and U
+        eigenvalues = eigenvalues[sorting_inds]
+        eigenvectors = eigenvectors[:, sorting_inds]
         
         # TODO 6: Select the top L eigenvectors and set A accordingly
         L = self.new_dim
-        self.A = None
+        self.A = eigenvectors[:, :L]
         
         return self
     
     # x_val is (m,n) matrix where each row is an n-dimensional vector of features
     def transform(self, x_val):
-        z_val = (x_val - self.μ) / self.σ
+        z_val = x_val - self.mean
         # TODO 7: Apply the transformation equation
-        return None
+        return np.dot(z_val, self.A)
     
     def inverse_transform(self, z_val):
         # TODO 8: Apply the inverse transformation equation (including destandardization)
-        return None
+        z_train = np.dot(z_val, self.A.T)
+        return z_train + self.mean
 
     def fit_transform(self, x_train):
         return self.fit(x_train).transform(x_train)
